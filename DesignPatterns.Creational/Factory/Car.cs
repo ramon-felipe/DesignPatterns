114716621a8f7ad98773d7bb8d5633b594cc4741
Ramon Felipe Alves de Arruda Silva <ramon.felipe@hotmail.com>
2022-09-12 22:01:28 -0300

    (feat): factory method

---
+++ DesignPatterns.Creational/Factory/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory
+{
+    public abstract class Car
+    {
+        public Car()
+        {
+            Console.WriteLine("Creating a car...");
+        }
+    }
+
+    public class Couple : Car
+    {
+        public Couple()
+        {
+            Console.WriteLine("Couple car created!");
+        }
+    }
+
+    public class Sport : Car
+    {
+        public Sport()
+        {
+            Console.WriteLine("Sport car created!");
+        }
+    }
+}
---
+++ DesignPatterns.Creational/Factory/CarFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory
+{
+    public class CarFactory : ICarFactory
+    {
+        public Car Create(CarTypes carType)
+        {
+            return carType switch
+            {
+                CarTypes.Couple => new Couple(),
+                CarTypes.Sport => new Sport(),
+                _ => throw new NotImplementedException("It is not possible to create this type of car yet!"),
+            };
+        }
+    }
+}
